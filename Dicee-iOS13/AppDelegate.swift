eec988c7cbe811858ec077242966113c223db649
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-05-15 03:54:49 -0300

    finaliza app que sorteia dados aleatóriamente

--- Dicee-iOS13/AppDelegate.swift
+++ Dicee-iOS13/AppDelegate.swift
@@ -5,7 +5,6 @@
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
-
 import UIKit
 
 @UIApplicationMain
--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    let images = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        let dice1 = Int.random(in: 0...5)
+        let dice2 = Int.random(in: 0...5)
+        diceImageViewOne.image = images[dice1]
+        diceImageViewTwo.image = images[dice2]
     }
-
-
+    
 }
 
